2bd78e5cba1517ffb63291db385e23e83a83a6f3
SebastianArriola <arriolasebastian@live.com>
2022-02-27 14:13:23 -0300

    Navbar Added

---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+
+const pages = ['Products', 'Contact', 'Info'];
+
+const NavBar = () => {
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ mr: 7, display: { xs: 'none', md: 'flex' } }}
+                    >
+                        Guitars
+                    </Typography>
+                    <Box sx={{ ml: 140, flexGrow: 1, display: { xs: 'none', md: 'flex' } }} >
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+export default NavBar;
