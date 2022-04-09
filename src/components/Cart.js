928e9602c85780b5ec083b13414f14414c6ac0a2
SebastianArriola <arriolasebastian@live.com>
2022-04-08 21:14:59 -0300

    Add logo and fixed responsive css

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,7 +7,7 @@ import { db } from './firebaseConfig';
 import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
 import EditIcon from '@material-ui/icons/Edit';
 import CheckIcon from '@material-ui/icons/Check';
-import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 const Cart = () => {
 
@@ -36,155 +36,121 @@ const Cart = () => {
     setForm({
       ...form,
       [target.name]: target.value
-
     })
 
   }
 
   const handleLogin = (e) => {
-
     e.preventDefault();
     setRegistered(true);
-
   }
 
   const handleInc = (stock) => {
-    
-
-        setCount(count + 1)
-
-    
-
+    setCount(count + 1)
   }
 
   const handleDec = (stock) => {
-
-    
-
-        setCount(count - 1);
-
-    
-
-
+    setCount(count - 1);
   }
 
-  const handleEdit = (i, quantity) =>{
-
+  const handleEdit = (i, quantity) => {
     setCount(quantity);
     let aux = [...edit];
     aux[i] = true;
     setEdit(aux);
-
   }
 
-  const updateCart = (item, quantity, i) =>{
-
+  const updateCart = (item, quantity, i) => {
     let aux = [...edit];
     aux[i] = false;
-    editCart(item,quantity);
+    editCart(item, quantity);
     setEdit(aux);
-
   }
 
   const handleConfirm = () => {
-
     let order = {
       buyer: form,
-
       items: cart,
       date: serverTimestamp(),
       total: total
 
     }
-
     const ordersCollection = collection(db, "orders");
     const qorder = addDoc(ordersCollection, order);
-
     qorder
       .then((resp) => {
-
         setNumberBuy(resp.id);
-
       })
       .catch((err) => {
-
         console.log(err);
-
       })
 
   }
 
+
   return (
     <div className='cart__container animate__animated animate__fadeIn'>{cart.length !== 0 ? cart.map((item, i) => {
-
       return (<div key={item.item.id} className='cart__grid'>
         <img className='cart__img' src={item.item.pictureUrl} alt="product_image"></img>
         <p className='cart__title'>{item.item.title}</p>
         <p className='cart__price'>{"$" + item.item.price}</p>
         <p className='cart__cant'>U.{item.quantity}</p>
         <p className='cart__subtotal'>{"$" + item.item.price * item.quantity}</p>
-        {!edit[i] ? <Button onClick={()=>handleEdit(i, item.quantity)} size="small"><EditIcon/></Button> : <div className="count__edit__grid"><Button onClick={handleDec}>-</Button><span>{count}</span><Button onClick={handleInc}>+</Button><Button className='check__center' onClick={() => { updateCart(item.item, count, i) }}><CheckIcon /></Button></div>}
-        <Button style={{maxWidth: '10rem', minWidth: '4rem', textAlign:"left"}} onClick={() => { removeItem(item.item) }}><DeleteRoundedIcon/></Button>
+        {!edit[i] ? <Button onClick={() => handleEdit(i, item.quantity)} size="small"><EditIcon /></Button> : <div className="count__edit__grid"><Button onClick={handleDec}>-</Button><span>{count}</span><Button onClick={handleInc}>+</Button><Button className='check__center' onClick={() => { updateCart(item.item, count, i) }}><CheckIcon /></Button></div>}
+        <Button style={{ maxWidth: '10rem', minWidth: '4rem', textAlign: "left" }} onClick={() => { removeItem(item.item) }}><DeleteRoundedIcon /></Button>
       </div>
       )
 
 
     }) : <>{numberBuy === undefined ? <><p className="cart-empty">Su carrito esta vacio.</p>
-    <Box textAlign="center" margin={5}><Link className="cart-link" to="/">Volver al catalogo</Link></Box></> : <><p className="cart-empty">Su compra fue realizada, numero de compra: {numberBuy}.</p>
-    <Box textAlign="center" margin={5}><Link className="cart-link" to="/">Volver al catalogo</Link></Box></>}</>}
+      <Box textAlign="center" margin={5}><Link className="cart-link" to="/">Volver al catalogo</Link></Box></> : <><p className="cart-empty">Su compra fue realizada, numero de compra: {numberBuy}.</p>
+      <Box textAlign="center" margin={5}><Link className="cart-link" to="/">Volver al catalogo</Link></Box></>}</>}
 
       {cart.length !== 0 && <><p className='cart__total'>TOTAL: {"$" + total}</p>
         <Box textAlign="right"><Button onClick={clear} variant="outlined"><span className='cart__clear'>LIMPIAR CARRITO</span></Button></Box>
         {registered ? <> {numberBuy === undefined && <><p className='cart__buy'>Registro exitoso, ya puede confirmar la compra!</p><Box textAlign={"center"}><Button variant='outlined' onClick={() => { handleConfirm(); clear(); }}>Confirmar compra</Button></Box></>}</> : <><p>Para confirmar la compra, debe dejar sus datos</p>
           <ValidatorForm className="form__contenedor" onSubmit={handleLogin}>
-          <div className='form__div'>
-
-          <TextValidator
-                    label="Nombre"
-                    onChange={handleInputChange}
-                    name="name"
-                    value={name}
-                    className="form__field"
-                    validators={['required']}
-                    errorMessages={['Este campo es obligatorio.']}
-                    autoComplete="off"
-                />
-
-
-          </div>
-          <div className='form__div'>
-          <TextValidator
-                    label="Telefono"
-                    onChange={handleInputChange}
-                    name="phone"
-                    value={phone}
-                    className="form__field"
-                    validators={['required', 'isNumber']}
-                    errorMessages={['Este campo es obligatorio.', 'Debe ingresar un numero']}
-                    autoComplete="off"
-                />
+            <div className='form__div'>
+              <TextValidator
+                label="Nombre"
+                onChange={handleInputChange}
+                name="name"
+                value={name}
+                className="form__field"
+                validators={['required']}
+                errorMessages={['Este campo es obligatorio.']}
+                autoComplete="off"
+              />
             </div>
             <div className='form__div'>
-            <TextValidator
-                    label="Email"
-                    onChange={handleInputChange}
-                    name="email"
-                    value={email}
-                    className="form__field"
-                    validators={['required', 'isEmail']}
-                    errorMessages={['Este campo es obligatorio.', 'Este email no es valido']}
-                    autoComplete="off"
-                />
+              <TextValidator
+                label="Telefono"
+                onChange={handleInputChange}
+                name="phone"
+                value={phone}
+                className="form__field"
+                validators={['required', 'isNumber']}
+                errorMessages={['Este campo es obligatorio.', 'Debe ingresar un numero']}
+                autoComplete="off"
+              />
+            </div>
+            <div className='form__div'>
+              <TextValidator
+                label="Email"
+                onChange={handleInputChange}
+                name="email"
+                value={email}
+                className="form__field"
+                validators={['required', 'isEmail']}
+                errorMessages={['Este campo es obligatorio.', 'Este email no es valido']}
+                autoComplete="off"
+              />
             </div>
             <Box mt={3}>
-            <Button type="submit" variant='outlined'>Registrarse</Button>
+              <Button type="submit" variant='outlined'>Registrarse</Button>
             </Box>
           </ValidatorForm></>}
-
       </>}
-
-
-
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,47 +1,28 @@
 import * as React from 'react';
 import CardWidget from './CardWidget';
 import { Link } from 'react-router-dom';
+import logo from '../img/logo1.png'
 
 
 export const pages = ['Electricas', 'Acusticas', 'Clasicas'];
 
 const NavBar = () => {
 
-
-
     return (
         <>
             <header className='header'>
-
                 <div className='header__container'>
-
                     <div className='header__barra animate__animated animate__fadeIn'>
-
-
-                        <Link to="/" className='navegacion__enlace'>ANASHE</Link>
-
-
+                        <Link to="/" className='navegacion__enlace'><img className="img__logo" src={logo}/></Link>
                         <nav className='navegacion'>
-
                             <Link to="/" className='navegacion__enlace navegacion__inicio'>inicio</Link>
-
-                            {pages.map((page, index)=>{
-
-                                return <Link key={index} to={"category/"+(index+1)} className='navegacion__enlace'>{page}</Link>
-                                    
+                            {pages.map((page, index) => {
+                                return <Link key={index} to={"category/" + (index + 1)} className='navegacion__enlace'>{page}</Link>
                             })}
-
-                            <CardWidget/>
-                            
-                            
-
+                            <CardWidget />
                         </nav>
-
                     </div>
-
-
                 </div>
-
             </header>
         </>
     );
