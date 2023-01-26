7a338a5204c0e75c8544e5f6e7937851e9741306
Sebastian Arriola <sarriola@artech-consulting.com>
2023-01-26 17:23:54 -0300

    Fix links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
         <div className='animate__animated animate__fadeIn'>
             <div className='footer__contenido'>
             <p className='footer__name'>Sebastian Arriola</p>
-                <a href='https://www.linkedin.com/in/sebastian-arriola-a9a1b5224/' className='footer__link' target="_blank"><LinkedInIcon style={{width: 30, height: 30, color: "white"}}/></a>
-                <a href='https://github.com/SebastianArriola' className='footer__link' target="_blank"><GitHubIcon style={{width: 30, height: 30, color: "white"}}/></a>
-                <a href='mailto:arriolasm00@gmail.com?Subject=Contact%20from%20web%20ecommerce' className='footer__link' target="_blank"><EmailIcon style={{width: 30, height: 30, color: "white"}}/></a>
+                <a href='https://www.linkedin.com/in/sebastian-arriola-a9a1b5224/' rel="noreferrer" className='footer__link' target="_blank"><LinkedInIcon style={{width: 30, height: 30, color: "white"}}/></a>
+                <a href='https://github.com/SebastianArriola' className='footer__link' rel="noreferrer" target="_blank"><GitHubIcon style={{width: 30, height: 30, color: "white"}}/></a>
+                <a href='mailto:arriolasm00@gmail.com?Subject=Contact%20from%20web%20ecommerce' rel="noreferrer" className='footer__link' target="_blank"><EmailIcon style={{width: 30, height: 30, color: "white"}}/></a>
             </div>
         </div>
     )
